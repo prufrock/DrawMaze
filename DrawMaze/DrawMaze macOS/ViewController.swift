81552e3ab4b0d362232857432da0c0e419db712e
David Kanenwisher <d.kanen@gmail.com>
2022-10-24 22:17:19 -0500

    Add macOS target

---
+++ DrawMaze/DrawMaze macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DrawMaze macOS
+//
+//  Created by David Kanenwisher on 10/24/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
